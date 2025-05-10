0f1b2f3d187d0b8d86f3e3a57786b6da494f6d6a
Lakindu <lakinduperera458@gmail.com>
2025-05-10 09:29:27 +0530

    implement the custom hook

--- services/api.ts
+++ services/api.ts
@@ -28,18 +28,3 @@ export const fetchMovies = async ({ query}: {query: string}) =>{
     return data.results;
 }
 
-
-
-// const url = 'https://api.themoviedb.org/3/keyword/keyword_id/movies?include_adult=false&language=en-US&page=1';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NmZkOWIxOTMxOTQ5NDNmMDMyODEzMTdlMGFmMTk2OSIsIm5iZiI6MTc0Njc1OTMyNi40NjgsInN1YiI6IjY4MWQ2ZTllOWNkMjZiOTNhZTkzYjFiZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.T1qe_nCqjsAPtMvwJ5SAGxeiW5jQgydXX6QsPwvpIm8'
-//   }
-// };
-
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error(err));
