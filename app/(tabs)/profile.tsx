a87dbde62df7c72ff8781edef7d02cf22136a1c4
Lakindu <lakinduperera458@gmail.com>
2025-05-24 23:19:05 +0530

    Implement the start the profile and save pages

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
+import { icons } from '@/constants/icons'
 
 const profile = () => {
   return (
-    <View>
-      <Text></Text>
+    <View className='bg-primary flex-1 px-10'>
+      <View className='flex justify-center items-center flex-1 flex-col gap-5 '>
+        <Image source={icons.person} className='size-10' tintColor="#Fff" />
+        <Text className='text-white text-2xl font-bold'>Profile</Text>
+      </View>
     </View>
   )
 }
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,10 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { icons } from '@/constants/icons';
 
 const saved = () => {
   return (
-    <View>
-      <Text></Text>
+    <View className='bg-primary flex-1 px-10'>
+      <View className='flex justify-center items-center flex-1 flex-col gap-5 '>
+        <Image source={icons.save} className='size-10' tintColor="#Fff" />
+        <Text className='text-white text-2xl font-bold'>Save</Text>
+      </View>
     </View>
   )
 }
